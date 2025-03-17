0228648f8fdbcceffec984c20e7d8626e08fd33b
Dean14692-prog <dennismuasyangui@gmail.com>
2025-03-17 08:59:31 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+// Function to create thank you cards
+function writeCards(names, event) {
+  let messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return messages;
+}
+
+// Function to count down to zero
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+
+// Export functions for testing
+module.exports = { writeCards, countDown };
